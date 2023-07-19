83677d4834a40d5b60ed47142c58a28d2d833101
Miguel Urena <urena_miguel82@outlook.com>
2023-07-18 19:46:09 -0700

    Add function for button

---
+++ script.js
@@ -0,0 +1,16 @@
+// To start the game
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+
+}
+
+// To reset the game if the game ends in a tie
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', resetGame);
+
+function resetGame() {
+  
+}
