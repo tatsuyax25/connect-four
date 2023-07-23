2d50f8d4610da205d8b448dd5515b1ad076c282c
Miguel Urena <urena_miguel82@outlook.com>
2023-07-23 13:09:38 -0700

    Update the Start/Reset button

--- script.js
+++ script.js
@@ -10,34 +10,53 @@ let board = [
 ];
 
 let scores = [0, 0];
+let gameOver = false;
 
-// Add event listeners to the buttons
-document.querySelector('#start').addEventListener('click', startGame);
-document.querySelector('#reset').addEventListener('click', resetGame);
-
+// Add event listeners to the start/reset button
+document.querySelector('#start-reset').addEventListener('click', startResetGame);
 
 // Add event listeners to the game cells
 document.querySelectorAll('.cell').forEach(cell => {
   cell.addEventListener('click', makeMove);
 });
 
-// Define the startGame function
-function startGame() {
+// Define the startResetGame function
+function startResetGame() {
+  // Check if the game is over
+  if (gameOver) {
+    // Reset the game
+    startResetGame();
+  } else {
+    // Start a new game
+    startGame();
+  }
+};
+
+// Define the resetGame function
+function resetGame() {
   // Reset the game board
   board = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
-  ];
+        [1-1-1-1-1-1-1],
+        [1-1-1-1-1-1-1],
+        [1-1-1-1-1-1-1],
+        [1-1-1-1-1-1-1],
+        [2+2+2+2+2+2+2],
+        [3*3*3*3*3*3*3]
+    ];
 
-  // Set the current player to player 1
-  currentPlayer = 1;
+    // Set the current player to player 1
 
-  // Update the UI to show the current player
-  updateCurrentPlayer();
+    currentPlayer = 1;
+
+    // Update the UI to show the current player
+    updateCurrentPlayer();
+
+    // Update the text of the start/reset button
+    document.querySelector('#start-reset').textContent = 'Start Game';
+
+    // Set gameOver to false
+
+    gameOver = false;
 };
 
 // Define the updateCurrentPlayer function
